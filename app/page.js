1f92a55c55a4c59dd0865cea056097e8bac321c9
Elmorsy11 <egypt.team.dev>
2023-09-21 12:15:03 +0200

    sd

--- app/page.js
+++ app/page.js
@@ -2,24 +2,14 @@
 'use client'
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
-// export default class page extends Component {
-//   render() {
-//     return (
-//       <main>
-//         <SwaggerUI url="/api/swagger" />
+export default class page extends Component {
+  render() {
+    return (
+      <main>
+        <SwaggerUI url="/api/swagger" />
 
-//       </main>
-//     )
-//   }
-// }
-
-import React from 'react'
-
-const page = () => {
-  return (
-    <SwaggerUI url="/api/swagger" />
-
-  )
+      </main>
+    )
+  }
 }
 
-export default page
