6d5f18be6fa6ed22743b130e8b88d58aa2c235fe
Elmorsy11 <egypt.team.dev>
2023-09-25 13:59:04 +0200

    add logo

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -5,9 +5,12 @@ const NavBar = ({ isAuth, setIsAuth }) => {
          transition-[background,color,box-shadow] duration-700  
         '  >
             <div className="container">
-                <button onClick={() => setIsAuth(!isAuth)} className='backdrop-blur-lg  bg-[#736FB2] hover:opacity-80 transition-[background] duration-700 p-2 text-white rounded-md   text-sm w-[80px]'>
-                    Log out
-                </button>
+                <div className="flex items-center justify-between w-full">
+                    <h2 className="font-semibold text-[#736FB2] text-lg">  S W A G G E R </h2>
+                    <button onClick={() => setIsAuth(!isAuth)} className='backdrop-blur-lg  bg-[#736FB2] hover:opacity-80 transition-[background] duration-700 p-2 text-white rounded-md   text-sm w-[80px]'>
+                        Log out
+                    </button>
+                </div>
             </div>
 
         </nav >
